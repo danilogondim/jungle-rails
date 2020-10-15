93ef2d189c3fe2ad357faaf6f8a31658227dd318
Danilo Gondim <daniloencarnacao@gmail.com>
2020-10-15 21:19:25 +0000

    Add sessions controller, login view and sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   get 'about' => 'about#show'
+
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
